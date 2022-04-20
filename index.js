06c0c669ceaf1816d517bd29a45f481d73cbc33b
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-04-20 16:46:32 -0400

    commit tree 601701dca3bdeeea7248d3e76dbeee1549187223

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 // Write your solution here!
-const pets = ['Milo', 'Otis', 'Garfield'];
-const copyOfPets1 = pets.slice();
-const addToLast = copyOfPets.push('Odie');
-const append = copyOfPets1;
-append;
-const copyOfPets2 = pets.slice();
-const last_pet = copyOfPets2.pop();
-const prepend = copyOfPets2.unshift(last_pet);
+const append = ['Milo', 'Otis', 'Garfield'];
+append.push("Odie");
+console.log(append);
+
+const prepend = ['Milo', 'Otis', 'Garfield']
+prepend.unshift("Odie");
+console.log(prepend);
+
+const removeLast = ['Milo', 'Otis', 'Garfield']
+removeLast.pop();
+console.log(removeLast);
 
 
+const removeFirst = ['Milo', 'Otis', 'Garfield']
+removeFirst.shift('Milo');
+console.log(removeFirst);
 
 
 
